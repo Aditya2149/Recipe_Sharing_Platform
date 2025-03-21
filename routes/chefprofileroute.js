2058115c9d622ed540b4028f379ce834259655f7
Aditya Kumar <94384575+Aditya2149@users.noreply.github.com>
2025-03-22 01:01:30 +0530

    updated chef profile

--- routes/chefprofileroute.js
+++ routes/chefprofileroute.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const {chefProfileController, getAllChefs, getTopRatedChefs} = require('../controllers/chefprofilecontroller');
+const chefProfileController = require('../controllers/chefprofilecontroller');
 const authMiddleware = require('../middlewares/authMiddleware');
 const router = express.Router();
 
 // Route to get all chefs
-router.get("/all", getAllChefs);
+router.get("/all", chefProfileController.getAllChefs);
 
 // Route to get top-rated chefs
-router.get("/top-rated", getTopRatedChefs);
+router.get("/top-rated", chefProfileController.getTopRatedChefs);
 
 // Get a chef's profile
 router.get('/:chefId', chefProfileController.getChefProfile);
